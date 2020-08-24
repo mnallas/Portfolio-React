98bc000141ff1ede59b92f88163897f52d190c5a
myhkas <mmyhkas@gmail.com>
2020-08-24 11:40:51 -0700

    Updates

---
+++ precache-manifest.ba8426f1735daef0ed21de82a5a63df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01b4a66b2d162a0f98439441a934e2a",
+    "url": "/Portfolio-React/index.html"
+  },
+  {
+    "revision": "916373c310631aac2083",
+    "url": "/Portfolio-React/static/css/main.15708bbc.chunk.css"
+  },
+  {
+    "revision": "82ae99bbb3e0e22547f4",
+    "url": "/Portfolio-React/static/js/2.11283df0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio-React/static/js/2.11283df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "916373c310631aac2083",
+    "url": "/Portfolio-React/static/js/main.81be1bea.chunk.js"
+  },
+  {
+    "revision": "3c54fa475694456d8909",
+    "url": "/Portfolio-React/static/js/runtime-main.5f5bde22.js"
+  }
+]);
